c2da331339fd617df821935ed9277075ef74bc64
slemeshaev <lemeshaevs@gmail.com>
2021-01-25 10:22:56 +0300

    Settings HomeNavigationStackView

--- DreamMeeting/DreamMeeting/View/HomeNavigationStackView.swift
+++ DreamMeeting/DreamMeeting/View/HomeNavigationStackView.swift
@@ -5,4 +5,36 @@
 //  Created by Станислав Лемешаев on 25.01.2021.
 //
 
-import Foundation
+import UIKit
+
+class HomeNavigationStackView: UIStackView {
+    
+    // MARK: - Properties
+    
+    let settingsButton = UIButton(type: .system)
+    let messageButton = UIButton(type: .system)
+    let dreamMeetingIcon = UIImageView(image: #imageLiteral(resourceName: "app_icon"))
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let topNavigationBar = [settingsButton, UIView(),
+                                dreamMeetingIcon, UIView(),
+                                messageButton]
+        
+        topNavigationBar.forEach { view in
+            addArrangedSubview(view)
+        }
+        
+        distribution = .equalCentering
+        isLayoutMarginsRelativeArrangement = true
+        layoutMargins = .init(top: 0, left: 16, bottom: 0, right: 16)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
