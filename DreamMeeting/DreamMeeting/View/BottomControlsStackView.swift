53f48b3894b3564cb7dcef58b979a3b02802825d
slemeshaev <lemeshaevs@gmail.com>
2021-01-26 10:21:57 +0300

    Creating bottom controls stack view

--- DreamMeeting/DreamMeeting/View/BottomControlsStackView.swift
+++ DreamMeeting/DreamMeeting/View/BottomControlsStackView.swift
@@ -5,4 +5,42 @@
 //  Created by Станислав Лемешаев on 26.01.2021.
 //
 
-import Foundation
+import UIKit
+
+class BottomControlsStackView: UIStackView {
+    
+    // MARK: - Properties
+    
+    let refreshButton = UIButton(type: .system)
+    let dislikeButton = UIButton(type: .system)
+    let superlikeButton = UIButton(type: .system)
+    let likeButton = UIButton(type: .system)
+    let boostButton = UIButton(type: .system)
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        heightAnchor.constraint(equalToConstant: 100).isActive = true
+        // distribution = .fillEqually
+        
+        refreshButton.setImage(#imageLiteral(resourceName: "refresh_circle").withRenderingMode(.alwaysOriginal), for: .normal)
+        dislikeButton.setImage(#imageLiteral(resourceName: "dismiss_circle").withRenderingMode(.alwaysOriginal), for: .normal)
+        superlikeButton.setImage(#imageLiteral(resourceName: "super_like_circle").withRenderingMode(.alwaysOriginal), for: .normal)
+        likeButton.setImage(#imageLiteral(resourceName: "like_circle").withRenderingMode(.alwaysOriginal), for: .normal)
+        boostButton.setImage(#imageLiteral(resourceName: "boost_circle").withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        let bottomControlsBar = [refreshButton, dislikeButton, superlikeButton, likeButton, boostButton]
+        
+        bottomControlsBar.forEach { view in
+            addArrangedSubview(view)
+        }
+        
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
