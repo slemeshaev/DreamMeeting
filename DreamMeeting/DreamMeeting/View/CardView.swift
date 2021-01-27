cc5f2a36d1f260607918e445c14540aba1592d14
slemeshaev <lemeshaevs@gmail.com>
2021-01-27 11:31:12 +0300

    Setup castom cardview

--- DreamMeeting/DreamMeeting/View/CardView.swift
+++ DreamMeeting/DreamMeeting/View/CardView.swift
@@ -5,4 +5,33 @@
 //  Created by Станислав Лемешаев on 27.01.2021.
 //
 
-import Foundation
+import UIKit
+
+class CardView: UIView {
+    
+    // MARK: - Properties
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = #imageLiteral(resourceName: "jane1")
+        return imageView
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemPurple
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
