9e5d1adf7ef7b1c357451bf93823bf628c43f5a0
slemeshaev <lemeshaevs@gmail.com>
2021-02-16 12:19:40 +0300

    init for auth button

--- DreamMeeting/DreamMeeting/Helpers/AuthButton.swift
+++ DreamMeeting/DreamMeeting/Helpers/AuthButton.swift
@@ -8,5 +8,15 @@
 import UIKit
 
 class AuthButton: UIButton {
-    // 
+    
+    init(title: String, type: ButtonType) {
+        super.init(frame: .zero)
+        
+        setTitle(title, for: .normal)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
