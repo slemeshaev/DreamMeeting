2ab45cfeae42952eeb57d75daaa523556c262d04
slemeshaev <lemeshaevs@gmail.com>
2021-02-17 15:26:31 +0300

    text fields sign up controller

--- DreamMeeting/DreamMeeting/Controller/Authentication/SignUpController.swift
+++ DreamMeeting/DreamMeeting/Controller/Authentication/SignUpController.swift
@@ -19,6 +19,16 @@ class SignUpController: UIViewController {
         return button
     }()
     
+    private let emailTextField = CustomTextFiled(placeholder: "Email")
+    private let fullNameTextField = CustomTextFiled(placeholder: "Полное имя")
+    private let passwordTextField = CustomTextFiled(placeholder: "Пароль",
+                                                    isSecureText: true)
+    private let authButton: AuthButton = {
+        let button = AuthButton(title: "Зарегистрироваться", type: .system)
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -32,15 +42,32 @@ class SignUpController: UIViewController {
         print(#function)
     }
     
+    @objc func handleSignUp() {
+        print(#function)
+    }
+    
     // MARK: - Helpers
     
     func configureUI() {
-        view.backgroundColor = .systemPurple
+        configureGradientLayer()
         
         view.addSubview(selectPhotoButton)
         selectPhotoButton.setDimensions(height: 275, width: 275)
         selectPhotoButton.centerX(inView: view)
         selectPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 8)
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, fullNameTextField,
+                                                       passwordTextField, authButton])
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        
+        view.addSubview(stackView)
+        stackView.anchor(top: selectPhotoButton.bottomAnchor,
+                         left: view.leftAnchor,
+                         right: view.rightAnchor,
+                         paddingTop: 24,
+                         paddingLeft: 32,
+                         paddingRight: 32)
     }
     
     
