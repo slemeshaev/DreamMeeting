c6108e1904a182ec54fbd4989b9c710f6946c8c5
slemeshaev <lemeshaevs@gmail.com>
2021-02-17 14:47:50 +0300

    select photo button

--- DreamMeeting/DreamMeeting/Controller/Authentication/SignUpController.swift
+++ DreamMeeting/DreamMeeting/Controller/Authentication/SignUpController.swift
@@ -11,11 +11,25 @@ class SignUpController: UIViewController {
     
     // MARK: - Properties
     
+    private let selectPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
+        button.addTarget(self, action: #selector(handleSelectPhoto), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+    }
+    
+    // MARK: - Actions
+    
+    @objc func handleSelectPhoto() {
+        print(#function)
     }
     
     // MARK: - Helpers
