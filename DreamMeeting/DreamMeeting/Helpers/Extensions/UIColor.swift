bbe3f84a9fec68f31e88bd8cf0ef77d1a20be971
slemeshaev <lemeshaevs@gmail.com>
2021-03-09 16:15:55 +0300

    icons

---
+++ DreamMeeting/DreamMeeting/Helpers/Extensions/UIColor.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor.swift
+//  DreamMeeting
+//
+//  Created by Станислав Лемешаев on 09.03.2021.
+//
+
+import Foundation
---
+++ DreamMeeting/DreamMeeting/Helpers/Extensions/UIViewController.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController.swift
+//  DreamMeeting
+//
+//  Created by Станислав Лемешаев on 09.03.2021.
+//
+
+import Foundation
